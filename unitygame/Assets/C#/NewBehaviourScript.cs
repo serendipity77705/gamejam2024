1712800e4bd1d94255970e628e33bd447c7899e2
Malk Daboor <malk.daboor@dal.ca>
2025-01-25 16:20:29 -0400

    created a bubble movement file

--- unitygame/Assets/C#/NewBehaviourScript.cs
+++ unitygame/Assets/C#/NewBehaviourScript.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    
+    Vector2 pos = new Vector2(10,2);
+    Transform obj;
     void Start()
     {
         Debug.Log("Hello people");
+        Debug.Log("What's uppppp");
+
+        transform.position = pos;
     }
 
     void Update()
     {
-        
+        pos.x += Input.GetAxis("Horizontal");
+        transform.position = pos;
     }
 }
 
